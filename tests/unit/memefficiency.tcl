4195b17bdd179b1ae29346c689577f25fcfa2580
Oran Agra <oran@redislabs.com>
2020-02-25 13:01:52 +0200

    fix github actions failing latency test for active defrag
    seems that github actions are slow, using just one client to reduce
    false positives.
    
    also adding verbose, testing only on latest ubuntu, and building on
    older one.
    
    when doing that, i can reduce the test threshold back to something saner

--- tests/unit/memefficiency.tcl
+++ tests/unit/memefficiency.tcl
@@ -100,11 +100,7 @@ start_server {tags {"defrag"}} {
                 assert {$frag < 1.1}
                 # due to high fragmentation, 100hz, and active-defrag-cycle-max set to 75,
                 # we expect max latency to be not much higher than 7.5ms but due to rare slowness threshold is set higher
-<<<<<<< HEAD
                 assert {$max_latency <= 30}
-=======
-                assert {$max_latency <= 60}
->>>>>>> 62adabd0... Fix latency sensitivity of new defrag test
             } else {
                 set _ ""
             }
@@ -230,11 +226,7 @@ start_server {tags {"defrag"}} {
                 assert {$frag < 1.1}
                 # due to high fragmentation, 100hz, and active-defrag-cycle-max set to 75,
                 # we expect max latency to be not much higher than 7.5ms but due to rare slowness threshold is set higher
-<<<<<<< HEAD
                 assert {$max_latency <= 30}
-=======
-                assert {$max_latency <= 60}
->>>>>>> 62adabd0... Fix latency sensitivity of new defrag test
             }
             # verify the data isn't corrupted or changed
             set newdigest [r debug digest]
@@ -325,7 +317,7 @@ start_server {tags {"defrag"}} {
                 assert {$frag < 1.1}
                 # due to high fragmentation, 100hz, and active-defrag-cycle-max set to 75,
                 # we expect max latency to be not much higher than 7.5ms but due to rare slowness threshold is set higher
-                assert {$max_latency <= 60}
+                assert {$max_latency <= 30}
             }
             # verify the data isn't corrupted or changed
             set newdigest [r debug digest]
